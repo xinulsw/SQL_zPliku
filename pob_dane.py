82ee2bad006a439cdf62cbb34b3bc127e8854071
Michał Krzemiński <michal_krzem@wp.pl>
2017-07-31 20:14:41 +0200

    SQL i wczytanie z pliku

---
+++ pob_dane.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+def pobierz_dane(plikcsv):
+	"""
+	funkcja zwraca tuplę tupli zawierajacych dane
+	z pliku csv do zapisania w tabeli"""
+
+	dane = [] #deklarujemy pustą liste
+	if os.path.isfile(plikcsv): #sprawdzamy czy plik istnieje na dysku
+		with open(plikcsv, "r") as zawartosc: #otwieramy plik do odczytu
+			zawartosc.readline()
+			for linia in zawartosc:
+				#linia = linia.remove("\n","")#usuwamy znaki końca lini
+				#linia = linia.replace("\r", "")#usuwamy znaki konca lini
+				#linia = linia.replace("utf-8", "")#odczytujemy znaki jako utf-8
+				#dodajemy elementy do tupli a tuplę do listy
+				dane.append(tuple(linia.split(";")))
+	else:
+		print("Plik z danymi: ",plikcsv, " nie istnieje!")
+
+	return tuple(dane) # przeksztalcamy liste na tuple i zwracamy ja
+
+
