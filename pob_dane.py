986795640432aa13ed4ada4bb4f63a4f3ab54b88
lo1cgsan <lo1cgsan@gmail.com>
2017-08-09 20:54:50 +0200

    Popraw formatowanie, usuń znaki końca linii i puste dane

--- pob_dane.py
+++ pob_dane.py
@@ -1,25 +1,27 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
 
+
 def pobierz_dane(plikcsv):
-	"""
-	funkcja zwraca tuplę tupli zawierajacych dane
-	z pliku csv do zapisania w tabeli"""
+    """
+    funkcja zwraca tuplę tupli zawierajacych dane
+    z pliku csv do zapisania w tabeli"""
 
-	dane = [] #deklarujemy pustą liste
-	if os.path.isfile(plikcsv): #sprawdzamy czy plik istnieje na dysku
-		with open(plikcsv, "r") as zawartosc: #otwieramy plik do odczytu
-			zawartosc.readline()
-			for linia in zawartosc:
-				#linia = linia.remove("\n","")#usuwamy znaki końca lini
-				#linia = linia.replace("\r", "")#usuwamy znaki konca lini
-				#linia = linia.replace("utf-8", "")#odczytujemy znaki jako utf-8
-				#dodajemy elementy do tupli a tuplę do listy
-				dane.append(tuple(linia.split(";")))
-	else:
-		print("Plik z danymi: ",plikcsv, " nie istnieje!")
+    dane = []  # deklarujemy pustą liste
+    if os.path.isfile(plikcsv):  # sprawdzamy czy plik istnieje na dysku
+        with open(plikcsv, "r") as zawartosc:  # otwieramy plik do odczytu
+            zawartosc.readline()
+            for linia in zawartosc:
+                linia = linia.replace("\n", "")  # usuwamy znaki końca lini
+                linia = linia.replace("\r", "")  # usuwamy znaki konca lini
+                linia = linia[:-1]  # usuwamy znak ";" z końca linii
+                # odczytujemy znaki jako utf-8
+                linia = linia.replace("utf-8", "")
+                # dodajemy elementy do tupli a tuplę do listy
+                dane.append(tuple(linia.split(";")))
+    else:
+        print("Plik z danymi: ", plikcsv, " nie istnieje!")
 
-	return tuple(dane) # przeksztalcamy liste na tuple i zwracamy ja
-
-
+    return tuple(dane)  # przeksztalcamy liste na tuple i zwracamy ja
--- wczyt_dane.py
+++ wczyt_dane.py
@@ -1,36 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sqlite3
+from pob_dane import pobierz_dane
+
 con = sqlite3.connect('test.db')
 con.row_factory = sqlite3.Row
 cur = con.cursor()
 
 # najpierw musimy utworzyć tabele i relacje między nimi
-#tworzenie tabel
+# tworzenie tabel
 cur.execute("DROP TABLE IF EXISTS aktynom;")
 # execute dla jednego polecenia
 cur.execute("""
-	CREATE TABLE IF NOT EXISTS aktynom(
-		data varchar(200) ,
-		stacja integer ,
-		calkow float ,
-		rozpr float ,
-		bezposr float ,
-		kon varchar(20)
-	)""")
+CREATE TABLE IF NOT EXISTS aktynom(
+    data varchar(200),
+    stacja integer,
+    calkow float,
+    rozpr float,
+    bezposr float,
+    kon varchar(20)
+)""")
 
-from pob_dane import pobierz_dane
+
 aktyn = pobierz_dane('GDY_201705.csv')
-
 print(aktyn)
-cur.executemany(
-	'INSERT INTO aktynom (data, stacja, calkow, rozpr, bezposr,kon) VALUES(?,?,?,?,?,?)', aktyn
-	)
-cur.execute(
-	'SELECT * FROM aktynom'
-	)
 
-
+cur.executemany('INSERT INTO aktynom (data, stacja, calkow, rozpr, bezposr, kon) VALUES (?, ?, ?, ?, ?, ?)', aktyn)
+cur.execute('SELECT * FROM aktynom')
 
 wynik = cur.fetchall()
 for w in wynik:
-	print(w['data'], w['stacja'],w['calkow'], w['rozpr'],w['bezp'],w['kon'])
+    print(w['data'], w['stacja'], w['calkow'], w['rozpr'], w['bezp'], w['kon'])
